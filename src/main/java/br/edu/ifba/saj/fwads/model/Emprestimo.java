4f01c0be4ff92918376ba29dfbb78ca05fa95840
Laerciodmc7 <Laerciosdamacena2006@gmail.com>
2025-09-10 14:01:30 -0300

    modificando camada de modelo

---
+++ src/main/java/br/edu/ifba/saj/fwads/model/Emprestimo.java
@@ -0,0 +1,96 @@
+package br.edu.ifba.saj.fwads.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+
+public class Emprestimo extends AbstractEntity{
+
+    @Column
+    @NotNull
+    private Usuario usuario;
+    @Column
+    @NotNull
+    private Livro livroEmprestado;
+    @Column
+    @NotNull
+    private LocalDate dataEmprestimo;
+    @Column
+    @NotNull
+    private LocalDate dataDevolução;
+    @Column
+    @NotBlank
+    private String status;
+
+    public Emprestimo(Usuario usuario, Livro livroEmprestado, LocalDate dataEmprestimo, LocalDate dataDevolução, String status) {
+        this.usuario = usuario;
+        this.livroEmprestado = livroEmprestado;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolução = dataDevolução;
+        this.status = status;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivroEmprestado() {
+        return livroEmprestado;
+    }
+
+    public void setLivroEmprestado(Livro livroEmprestado) {
+        this.livroEmprestado = livroEmprestado;
+    }
+
+    public LocalDate getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(LocalDate dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public LocalDate getDataDevolução() {
+        return dataDevolução;
+    }
+
+    public void setDataDevolução(LocalDate dataDevolução) {
+        this.dataDevolução = dataDevolução;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo{" +
+                "usuario=" + usuario +
+                ", livroEmprestado=" + livroEmprestado +
+                ", dataEmprestimo=" + dataEmprestimo +
+                ", dataDevolução=" + dataDevolução +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
+
+
+
+
+
+
+
--- src/main/java/br/edu/ifba/saj/fwads/model/Usuario.java
+++ src/main/java/br/edu/ifba/saj/fwads/model/Usuario.java
@@ -4,9 +4,10 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 @Entity
 public class Usuario extends AbstractEntity {
@@ -14,20 +15,68 @@ public class Usuario extends AbstractEntity {
     @Column
     @NotBlank
     @Size(min = 5)
-    private String senha;
+    private String nomeCompleto;
     @Column
     @NotBlank
     @Size(min = 5)
-    private String login;
+    private String userName;
     @Column
-    @Email
-    private String email;
+    @NotNull
+    private LocalDate dataNascimento;
+    @Column
+    @NotBlank
+    private String cpf;
+    @Column
+    @NotBlank
+    private String senha;
+    @Column
+    @NotEmpty
+    private ArrayList<Emprestimo> emprestimos;
+    @Column
+    @NotNull
+    private int limiteEmprestimo;
 
-    public String getLogin() {
-        return login;
+
+    public Usuario(String nomeCompleto, String userName, LocalDate dataNascimento, String cpf, String senha, ArrayList<Emprestimo> emprestimos, int limiteEmprestimo) {
+        this.nomeCompleto = nomeCompleto;
+        this.userName = userName;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.senha = senha;
+        this.emprestimos = emprestimos;
+        this.limiteEmprestimo = limiteEmprestimo;
     }
-    public void setLogin(String login) {
-        this.login = login;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public String getSenha() {
@@ -38,18 +87,32 @@ public class Usuario extends AbstractEntity {
         this.senha = senha;
     }
 
-    public String getEmail() {
-        return email;
+    public ArrayList<Emprestimo> getEmprestimos() {
+        return emprestimos;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setEmprestimos(ArrayList<Emprestimo> emprestimos) {
+        this.emprestimos = emprestimos;
     }
+
+    public int getLimiteEmprestimo() {
+        return limiteEmprestimo;
+    }
+
+    public void setLimiteEmprestimo(int limiteEmprestimo) {
+        this.limiteEmprestimo = limiteEmprestimo;
+    }
+
     @Override
     public String toString() {
-        return "Usuario [senha=" + senha + ", login=" + login + ", email=" + email + "]";
+        return "Usuario{" +
+                "nomeCompleto='" + nomeCompleto + '\'' +
+                ", userName='" + userName + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", cpf='" + cpf + '\'' +
+                ", senha='" + senha + '\'' +
+                ", emprestimos=" + emprestimos +
+                ", limiteEmprestimo=" + limiteEmprestimo +
+                '}';
     }
-
-    
-
 }
